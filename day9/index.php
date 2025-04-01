277318a552220f985731e89d4f8ec59f4efa4dd6
Diar-Limani <diarlimani20@gmail.com>
2025-04-01 20:46:04 +0200

    day9

---
+++ day9/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="add.php"method="POST">
+        <input type="text" name="name" placeholder="enter your name">
+        <input type="text" name="surname" placeholder="enter your surname">
+        <input type="text" name="email" placeholder="enter your email">
+
+        <button type="button" name="submit">ADD</button>
+    </form>
+</body>
+</html>
