96030861c854c67332662dbd70f7fb0404fc04b1
Diar-Limani <diarlimani20@gmail.com>
2025-04-03 20:45:35 +0200

    day10

---
+++ day10/dashboard.php
@@ -0,0 +1,47 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    include_once("config.php");
+    $sql="select *from user* ";
+    $getuser=$conn->prepare($sql);
+    $getUser->execute();
+    $users=$getUsers->fetchAll();
+
+    
+
+    ?>
+
+    <table>
+        <thead>
+            <th>ID</th>
+            <th>name</th>
+            <th>surname</th>
+            <th>email</th>
+        </thead>
+
+        <tbody>
+            <?php
+             foreach($users as $user) {
+                ?>
+             <tr>
+                <td><?= $user['id']?></td>
+                <td><?= $user['name']?></td>
+                <td><?= $user['surname']?></td>
+                <td><?= $user['email']?></td>
+             </tr>
+             <?php } ?>
+
+
+        </tbody>
+    </table>
+
+    <a href="index.php">gp to indeksss</a>
+    
+</body>
+</html>
